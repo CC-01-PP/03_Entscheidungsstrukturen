1b44d12a4ce947745f3f81badd352f1b5635dfdb
Mia <mia.kamenetski@online.de>
2022-11-10 12:19:09 +0100

    Fallunterscheidung

--- script_01.js
+++ script_01.js
@@ -54,18 +54,50 @@ isJohnEqual = (ageJohn == ageMark);
 
 /************ IF - ELSE IF ************/
 
-if(isJohnOlder)
-{
-    console.log("John ist älter.");
-}
-// 1. Alternative .... n
-else if(isJohnEqual)
-{
-    console.log("John ist gleich alt.");
-}
-// evtl. weitere Alternative(n) else if(){}
-// gemeinsame Alternative
-else
-{
-    console.log("John ist jünger.");
+// if(isJohnOlder)
+// {
+//     console.log("John ist älter.");
+// }
+// // 1. Alternative .... n
+// else if(isJohnEqual)
+// {
+//     console.log("John ist gleich alt.");
+// }
+// // evtl. weitere Alternative(n) else if(){}
+// // gemeinsame Alternative
+// else
+// {
+//     console.log("John ist jünger.");
+// }
+
+
+/************ Fallunterscheidung / SWITCH|CASE 1 ************/
+
+const firstName = "Jane";
+let job;
+
+job = "driver";     // .. fährt TAXI! / UBER
+job = "diver";      // .. taucht im Rhein!
+job = "artist";     // .. malt ein Bild!
+job = "pilot";      // .. macht etwas anderes! --> default
+job = "teacher";    // .. unterrichtet!
+job = "instructor";  // .. unterrichtet!
+
+switch (job) {
+    case "driver":
+            console.log(firstName + " fährt TAXI")
+            break;
+        case  "diver":
+            console.log(firstName + " taucht im Rhein!")
+            break;
+        case  "artist":
+            console.log(firstName + " malt ein Bild!")
+            break;
+        case  "teacher":
+        case  "instructor":
+            console.log(firstName + " unterrichtet!")
+            break;
+    default:
+        console.log(firstName + " macht etwas anderes!")
+        break;
 }
